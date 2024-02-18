7df9591d571bef789b4cd2645e26869b531c5e90
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-18 18:45:06 +1100

    finished tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word)]
+
+    def is_anagram(self, other_word):
+        other_word = other_word.lower()
+        return other_word != self.word and sorted(other_word) == self.sorted_word
